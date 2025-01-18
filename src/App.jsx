9d87ab364861190b245d80775ebb01a9122001f7
dhrumilgandhi <dhrumilgandhi540@gmail.com>
2025-01-18 16:22:49 +0530

    Create Project with React

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Provider } from 'react-redux';
+import './App.css';
+import { store } from './app/store';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
+
+
+function App() {
+  return (
+    <>
+     <Provider store={store}>
+        <AddTodo />
+        <Todos />
+     </Provider>
+    
+    </>
+  )
+}
+
+export default App
